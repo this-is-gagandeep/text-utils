ec4a361730e1b20e7c039f1062502726ca710d79
GagandeepJ <gjodhawat8@gmail.com>
2023-10-05 20:51:10 +0530

    React Router Installing

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; 
+
+function App() {
+  return (
+   <> <Router>
+        <div className="bg"></div>
+        <div className="container">
+          <Navbar title= "TextUtils"/>
+          <Switch>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm heading="Enter your text here!"/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+export default function Navbar(props) {
+
+  const [myStyle, setmyStyle] = useState({
+    color:'white',
+    backgroundcolor: 'black',
+    padding:'20px'
+  })
+  const [navText, setnavText] = useState("Enable Light Mode")
+  
+  const toggleStyle = () =>{
+    if (myStyle.color === 'white'){
+      setmyStyle({
+        color:'black',
+        backgroundcolor: 'white'
+        ,padding:'20px'
+      })
+      setnavText("Enable Light mode")
+    }
+    else 
+    {
+      setmyStyle({
+        color:'white',
+        backgroundcolor: 'black'
+        ,padding:'20px'
+      })
+      setnavText("Enable Dark mode")
+    }
+  }
+
+  return (
+  <div>
+   <nav className="navbar navbar-expand-lg  navbar-dark shadow-5-strong border border-white mb-5 " style={myStyle}>
+     <div className="container-fluid ">
+          <a className="navbar-brand" href="/">{props.title}</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav black me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link "to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="colorMode">
+          <div onClick={toggleStyle} class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label class="form-check-label" for="flexSwitchCheckDefault"> <h5>{navText}</h5></label>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  )
+}
+
+Navbar.propTypes = {titile: PropTypes.string}
